5730f2d9cf8612c65874b92efe6accb32f8ca6a7
RAIANE CAMPOS JANUARIO <raianejanu@hotmail.com>
2015-03-01 19:43:56 -0300

    Alteração no Main

--- src/br/com/Kml/ProjetoExtra.java
+++ src/br/com/Kml/ProjetoExtra.java
@@ -5,17 +5,53 @@
  */
 package br.com.Kml;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 /**
  *
  * @author Raiane Campos
  */
 public class ProjetoExtra {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   public static void main(String[] args) throws SQLException, IOException {
+//
+//       kmlPonto();  
+//       banco();
+//        kmllinha();
+//
+
     }
+
+    public static void kmlPonto() throws SQLException, IOException {
+        KmlPonto kml = new KmlPonto();
+        kml.conversorArquivo("C:\\Users\\Raiane Campos\\SkyDrive\\Projeto kml\\Linhas e Pontos de Onibus (KML)\\2014 01 30 BASE GERAL.kml");
+        kml.imprimirDados("C:\\Projeto kml");
+
+    }
+
+    public static void kmllinha() throws SQLException, IOException {
+        KmlLinhas kml = new KmlLinhas();
+        kml.conversorArquivo("C:\\Users\\Raiane Campos\\SkyDrive\\Projeto kml\\Linhas e Pontos de Onibus (KML)\\02_04_2014_redetranscol.kml");
+
+    }
+//  public static void banco () throws SQLException {
+//  
+//     ConexaoBD conexao = new ConexaoBD();
+//        conexao.conecta();
+////        String sql = "insert into ponto(name,lat,lng,alt)values ('10008',-20.261679,-40.263702,11.42126)";
+////       int done=conexao.statement.executeUpdate(sql);
+//        conexao.executeSQL("select*from ponto");
+//        while (conexao.resultset.next()) {
+//            System.out.println("Id = " + conexao.resultset.getString("id"));
+//            System.out.println("Ponto = " + conexao.resultset.getString("name"));
+//            System.out.println("Longitude = " + conexao.resultset.getString("lng"));
+//            System.out.println("Latitude = " + conexao.resultset.getString("lat"));
+//            System.out.println("Altura = " + conexao.resultset.getString("alt"));
+//        }
+//        conexao.desconecta();
+//  
+//  }
+//
     
 }
